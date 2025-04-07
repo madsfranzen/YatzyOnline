15faea8d1fa91bb6bc0cbce40ed0d1c44f6eb736
Mads Franzen <madsfranzen@live.dk>
2025-04-07 10:32:52 +0200

    welcome

--- netlify/functions/get_users/get_users.mjs
+++ netlify/functions/get_users/get_users.mjs
@@ -4,7 +4,12 @@ const handler = async (event) => {
     try {
         const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
         const collection = database.collection("users"); 
-        const results = await collection.find({}).limit(10).toArray();
+        const results = await collection.find({}).toArray();
+
+        console.log(results);
+
+        console.log(`Fetched ${results.length} users`);
+
         return {
             statusCode: 200,
             body: JSON.stringify(results),
