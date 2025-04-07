b66d05d8a697502860db2345f12fff3e6af850f5
Mads Franzen <madsfranzen@live.dk>
2025-04-07 11:15:23 +0200

    changaed port

--- Server/yatzyserver.js
+++ Server/yatzyserver.js
@@ -3,7 +3,7 @@ console.log("SERVER STARTED");
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = 10000;
 
 app.get('/', (req, res) => {
   console.log("HELLO YATZY");
