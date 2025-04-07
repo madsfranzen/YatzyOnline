b76f0a1476ae4834ba49b6a80fe55a7222583e58
Mads Franzen <madsfranzen@live.dk>
2025-04-07 10:54:37 +0200

    express testing

--- Server/yatzyserver.js
+++ Server/yatzyserver.js
@@ -1,1 +1,19 @@
 console.log("SERVER STARTED");
+
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  console.log("HELLO YATZY");
+})
+
+
+app.get('/play', (req, res) => {
+  console.log("HELLO PLAY GAME");
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
