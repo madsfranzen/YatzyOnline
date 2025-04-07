7ea2fea79ebf65582b1eebbdd24d8fd73a2e1d09
Mads Franzen <madsfranzen@live.dk>
2025-04-07 11:19:06 +0200

    closing get

--- Server/yatzyserver.js
+++ Server/yatzyserver.js
@@ -7,11 +7,13 @@ const port = 10000;
 
 app.get('/', (req, res) => {
   console.log("HELLO YATZY");
+  res.send('Hello World!')
 })
 
 
 app.get('/play', (req, res) => {
   console.log("HELLO PLAY GAME");
+  res.send('Hello Play Game!')
 })
 
 app.listen(port, () => {
