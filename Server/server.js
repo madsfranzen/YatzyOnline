99cec19b28af1994cc3f2fd0be3ce57a928b02a9
Mads Franzen <madsfranzen@live.dk>
2025-04-28 17:42:02 +0200

    cors

--- Server/server.js
+++ Server/server.js
@@ -4,27 +4,31 @@ import connectToDatabase from "./config/dbConfig.js";
 import playerRoutes from "./routes/playerRoutes.js";
 import session from "express-session";
 import { CheckSession } from "./middleware/auth.js";
-import authRoutes from './routes/authRoutes.js'
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
 const app = express();
+const cors = require("cors");
 
 async function startServer() {
   try {
     await connectToDatabase();
 
     app.use(express.json());
+    app.use(cors());
 
-    app.use(session({
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: true
-    }))
+    app.use(
+      session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+      }),
+    );
 
-    app.use(CheckSession)
+    app.use(CheckSession);
 
-    app.use("/auth", authRoutes)
+    app.use("/auth", authRoutes);
 
     app.use("/api/players", playerRoutes);
 
