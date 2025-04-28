e4bd4f14df9e7a11a5cea4b28dd61f61c7a0e089
Mads Franzen <madsfranzen@live.dk>
2025-04-28 09:18:51 +0200

    noget server

--- Server/yatzyserver.js
+++ Server/yatzyserver.js
@@ -6,24 +6,23 @@ import connectToDatabase from "./database/db.js";
 const app = express();
 const port = 10000;
 
-app.get('/', (req, res) => {
+app.get("/", (_req, res) => {
   console.log("HELLO YATZY");
-  res.send('Hello World!')
-})
+  res.send("Hello World!");
+});
 
-app.get('/play', (req, res) => {
+app.get("/play", (_req, res) => {
   console.log("HELLO PLAY GAME");
-  res.send('Hello Play Game!')
-})
+  res.send("Hello Play Game!");
+});
 
-app.get('/users', async (req, res) => {  
+app.get("/users", async (_req, res) => {
   const db = await connectToDatabase();
-  const users = await db.collection('users').find().toArray();
+  const users = await db.collection("users").find().toArray();
   console.log(users);
   res.send(users);
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+  console.log(`Example app listening on port ${port}`);
+});
