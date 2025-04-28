5ca618f88aab441004647d98b18ca485e3004cc0
Mads Franzen <madsfranzen@live.dk>
2025-04-28 11:40:04 +0200

    fix this shit

--- Client/proxy.js
+++ Client/proxy.js
@@ -1,5 +1,10 @@
-const navbarright = document.getElementsByClassName('navbar-right');
-const loginButton = document.createElement("loginButton");
+// Get the navbar-right element (this is the first element with the 'navbar-right' class)
+const navbarright = document.getElementsByClassName('navbar-right')[0];
 
-navbarright.appendChild(loginButton)
+// Create a login button element
+const loginButton = document.createElement('button');
+loginButton.textContent = 'Login'; // Set the button text
+
+// Append the button to the navbar-right
+navbarright.appendChild(loginButton);
 
